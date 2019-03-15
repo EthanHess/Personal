8d3999c59aacf2b89a500fc9396056f933bac8da
Ethan Hess <ech1988@gmail.com>
2019-03-14 19:42:38 -0700

    Games aniation update

--- Personal-portfolio/ethans-page/src/components/ThreeScene.js
+++ Personal-portfolio/ethans-page/src/components/ThreeScene.js
@@ -4,10 +4,18 @@ import * as THREE from 'three';
 class ThreeScene extends Component{
 
   //Initial example https://medium.com/@colesayershapiro/using-three-js-in-react-6cb71e87bdf4
-  
-  componentDidMount(){
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
+
+componentDidMount(){
+    this.sceneSetup()
+}
+
+sceneSetup = () => {
+    // const width = this.mount.clientWidth
+    // const height = this.mount.clientHeight
+
+    const width = window.innerWidth
+    const height = window.innerHeight
+    
     //ADD SCENE
     this.scene = new THREE.Scene()
     //ADD CAMERA
@@ -17,18 +25,36 @@ class ThreeScene extends Component{
       0.1,
       1000
     )
-    this.camera.position.z = 4
+    this.camera.position.z = 5
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setClearColor('#000000')
-    this.renderer.setSize(width, height)
+    this.renderer.setClearColor('#000000') //Is BG, change?
+    this.renderer.setSize(width, height) //TODO update
     this.mount.appendChild(this.renderer.domElement)
     //ADD CUBE
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
+    const geometry = new THREE.BoxGeometry(1, 2, 2)
+    //'#433F81'
+    const material = new THREE.MeshBasicMaterial({ color: '#3879e2'})
     this.cube = new THREE.Mesh(geometry, material)
     this.scene.add(this.cube)
+
+    this.addSperes()
     this.start()
+}
+
+addSperes = () => {
+    const geometry = new THREE.SphereGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: '#d81c3b'})
+    this.sphere = new THREE.Mesh(geometry, material)
+    this.scene.add(this.sphere)
+}
+
+//TODO finish
+addCubes = () => {
+    const cubes = []
+    for (let cube in cubes) {
+        this.scene.add(cube)
+    }
 }
 
 componentWillUnmount(){
