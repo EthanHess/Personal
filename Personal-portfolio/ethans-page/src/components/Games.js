2e6966623a2bdc8863fe52bf47d8bc13749317bc
Ethan Hess <ech1988@gmail.com>
2019-03-22 17:36:18 -0700

    adds game images but needs resizing

--- Personal-portfolio/ethans-page/src/components/Games.js
+++ Personal-portfolio/ethans-page/src/components/Games.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import ThreeScene from './ThreeScene.js'; 
+import './Games.css'; 
+import Slider from 'react-slick';
+//TODO add TweenLite
 import axios from 'axios'; 
+
+//Images
+import imageOne from '..//GameImages/gameOne_1'; 
+import imageTwo from '..//GameImages/gameOne_2'; 
+import imageThree from '..//GameImages/gameOne_3'; 
+import imageFour from '..//GameImages/gameOne_4'; 
+import imageFive from '..//GameImages/gameOne_5'; 
 
 export default class Games extends Component {
     constructor() {
         super()
-        //TODO add props
+        this.state = {
+            imagesArray: []
+        }
+
+        this.setImages()
+    }
+
+    setImages = () => {
+        this.setState({ imagesArray : [imageOne, imageTwo, imageThree, imageFour, imageFive] })
     }
 
     componentDidMount() {
@@ -16,13 +34,52 @@ export default class Games extends Component {
 
     }
 
+    handleImageClick = (itemId) => {
+
+    }
+
     //TODO subclass three scene but this is a test
     render() {
+
+        //For slider
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1
+        }
+
+        //To populate slider
+        const { imagesArray } = this.state
+        const children = imagesArray.map((item, index) => {
+            return <div className="scroll_child" key={index} onTouchStart="this.classList.toggle('hover');">
+                <div className="flip_container">
+                    <div className="front">
+                        <div className="top_scroll_container">
+                            <img className="main_image" 
+                            onClick={() => this.handleImageClick(item.id)} 
+                            src={item}
+                            alt='event-card'/>
+                        </div>
+                    </div>
+                    <div className="back">
+
+                    </div>
+                </div>
+            </div>
+        })
+
         //Could use ID instead of class name if not going to reuse
         return (
-            // <div>
-            // </div>
-            <ThreeScene/> 
+            <div>
+                <ThreeScene/> 
+                <div className="animation_container_two">
+                    <Slider className="slick_slider" {...settings}>
+                        { children }
+                    </Slider>
+                </div>
+            </div>
         )
     }
 }
