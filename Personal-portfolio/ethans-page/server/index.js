81e9d0f58ec3db130ed41cd65c0e425942c0f096
Ethan Hess <ech1988@gmail.com>
2019-09-19 11:13:02 -0400

    auth 0 configuration

--- Personal-portfolio/ethans-page/server/index.js
+++ Personal-portfolio/ethans-page/server/index.js
@@ -3,14 +3,31 @@ const bodyParser = require('body-parser');
 const massive = require('massive');
 const session = require('express-session')
 
-//TODO add controllers + other packages
+// --- CONTROLLERS ---
+
+const authController = require('./controllers/authController') 
+
+// --- DATABASE ---
+
+require('dotenv').config()
+massive(process.env.CONNECTION_STRING).then(db => app.set('db', db))
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+}))
+app.use(express.static(`${__dirname}/../build`))
 
 // --- ENDPOINTS ---
 
 //Login/Register
-app.post('/api/auth/login', authController.login);
-app.post('/api/auth/signup', authController.register);
-app.post('/api/auth/logout', authController.logout);
+app.post('/api/auth/login', authController.login)
+app.post('/api/auth/signup', authController.register)
+app.post('/api/auth/logout', authController.logout)
 app.get('/api/auth/session', authController.getSession)
 
 //User (Client) Profile
@@ -20,3 +37,13 @@ app.get('/api/auth/session', authController.getSession)
 //Payments
 
 //Messages
+
+// --- SERVER ---
+
+//TODO check login
+
+const SERVER_PORT = process.env.SERVER_PORT;
+app.listen(SERVER_PORT, () => {
+  console.log('Backend of Project Portal listening on port ' + SERVER_PORT)
+})
+
--- Personal-portfolio/ethans-page/src/components/Login.js
+++ Personal-portfolio/ethans-page/src/components/Login.js
@@ -19,6 +19,9 @@ class Login extends Component {
 
     componentDidMount() {
         console.log('--- LOGIN COMPONENT MOUNTED ---')
+        
+        //TEST
+        setTimeout(this.authZeroHandler, 2000)
     }
 
     //TODO geocode address for location? (react-geocode)
@@ -53,6 +56,12 @@ class Login extends Component {
         } else {
             alert('Please enter sing up credentials')
         }
+    }
+
+    authZeroHandler = () => {
+        const redirectUri = encodeURIComponent(`${window.location.origin}/auth/callback`);
+        console.log('--- DOMAIN ---', process.env.REACT_APP_PROJECT_PORTAL_AUTH0_DOMAIN)
+        window.location = `https://${process.env.REACT_APP_PROJECT_PORTAL_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_PROJECT_PORTAL_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}&response_type=code`
     }
 
     monitorTextChangeUsername = (e) => {
