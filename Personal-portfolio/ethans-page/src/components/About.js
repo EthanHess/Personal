7acd5f303319496956254f3baf9d4655f727d1e9
ech1988@gmail.com <ech1988@gmail.com>
2020-05-13 19:17:38 -0700

    More GSAP boilerplate code

--- Personal-portfolio/ethans-page/src/components/About.js
+++ Personal-portfolio/ethans-page/src/components/About.js
@@ -3,59 +3,46 @@ import './About.css';
 import axios from 'axios'; 
 import YouTuber from 'youtube-embed-video'; 
 
-import { TweenLite } from "gsap/all";
+import { TimelineLite } from "gsap/all"; 
+//Imp. Tween Max
 
 export default class About extends Component {
     constructor() {
         super()
         this.state = {
             theme: "SPACE", 
-            animationViewArray: [], //Will map in render(), just have array of colors for UIView BG
-            gsapArray: [] //Animations, do we need this as an array?
           }
-  
-          this.animationArrayCreate()
+
+          //Can remove in state eventually now that we're using timeline
+          this.myTween = new TimelineLite({paused: true})
+          this.myElements = [];
     }
 
-    animationArrayCreate = () => {
-        const { animationViewArray } = this.state
-        if (animationViewArray.length == 0) {
-            for (var i = 0; i < 30; i++) {
-                animationViewArray.push(i); 
-            }
-        }
-        console.log("animations array", animationViewArray)
+    //TODO use
+    getRandomInt = (min, max) => {
+        return min + Math.floor(Math.random() * (max - min + 1));
     }
 
     componentDidMount() {
         console.log('--- ABOUT COMPONENT MOUNTED ---')
+        this.myTween.staggerTo(this.myElements, 0.5, {y: 0, autoAlpha: 1}, 0.1);
     }
 
     initAnimations = () => {
-
+        //https://greensock.com/forums/topic/14938-tween-through-array-for-values/
     }
 
     render() {
 
-        const { animationViewArray } = this.state
-
-        const divArray = animationViewArray.map((item, index) => {
-            return <div className={'animation_' + item}>{item}</div>
-        })
-
-        // const animations = divArray.map((item, index) => {
-        //     return TweenLite.to(item, 1, {x: 100, y: 100})
-        // })
-
-        // for (var i = 0; i < divArray.length; i++) {
-        //     const element = divArray[i]
-        //     TweenLite.to(element, 1, {x: 100, y: 100})
-        // }
-
-        return (
-            <div id="about_parent">
-                {divArray}
-            </div>
-        )
+        return <div>
+            <ul>
+                {this.myElements.map((element, index) => <li
+                    key={element.id}
+                    ref={li => this.myElements[index] = li}
+                >
+                {element.name}
+            </li>)}
+            </ul>
+        </div>;
     }
 }
